9a6f634dc9afb64b2a6c6e1a547f0f364129acc8
Hevans Vinicius Pereira <hevansv@gmail.com>
2023-04-04 19:22:36 -0300

    mensagem

--- projetos/calculadora/funcoes.py
+++ projetos/calculadora/funcoes.py
@@ -6,3 +6,12 @@ def soma(a,b):
         print("a e b devem ser numeros")
 
     return a+b
+
+def substracao(a,b):
+    try:
+        a = float(a)
+        b = float(b)
+    except TypeError:
+        print("a e b devem ser numeros")
+
+    return a-b
